7a70878afee6290cec601c98627674f6fc84ea13
Mineglitch <pavangerow@gmail.com>
2022-05-31 15:44:03 +0100

    Multi-Threaded

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,26 @@
 
 #define LED 2
 
-void setup(){
-  Serial.begin(115200);
+TaskHandle_t drive_core;
+
+void drive_core_code( void * parameter){
   motorInit();
+  for(;;){
+    rotCW(360);
+    delay(1000);
+  }
+}
+
+void setup(){
+
+  Serial.begin(115200);
+  
+  xTaskCreatePinnedToCore(drive_core_code, "drive", 1000, NULL, 0, &drive_core, 0);
+  
   cam_init();
 }
 
 void loop() {
-  //rotCW(360);
-  //delay(1000);
   read_values();
+  Serial.println(xPortGetCoreID());
 }
