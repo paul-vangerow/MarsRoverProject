310eb7bc4bf45fcd41f3b83af0b0a79301819239
abdalalkilani <94132373+abdalalkilani@users.noreply.github.com>
2022-06-02 20:46:06 +0100

    blue balls

--- VisionTesting/EEE_IMGPROC.v
+++ VisionTesting/EEE_IMGPROC.v
@@ -2,7 +2,7 @@ module EEE_IMGPROC(
 	// global clock & reset
 	clk,
 	reset_n,
-	
+
 	// mm slave
 	s_chipselect,
 	s_read,
@@ -17,17 +17,17 @@ module EEE_IMGPROC(
 	sink_ready,
 	sink_sop,
 	sink_eop,
-	
+
 	// streaming source
 	source_data,
 	source_valid,
 	source_ready,
 	source_sop,
 	source_eop,
-	
+
 	// conduit
 	mode
-	
+
 );
 
 
@@ -75,23 +75,61 @@ wire [7:0]   red_out, green_out, blue_out;
 
 wire         sop, eop, in_valid, out_ready;
 ////////////////////////////////////////////////////////////////////////
+///Declaring variables for HSV conversion
+wire[7:0] hue ;
+wire[7:0] saturation, value, min;
 
+
+/*
 // Detect red areas
 wire red_detect;
 assign red_detect = red[7] & ~green[7] & ~blue[7];
-
+*/
 // Find boundary of cursor box
 
+
+///Conversion from RGB to HSV
+assign value = (red > green) ? ((red > blue) ? red[7:0] : blue[7:0]) : (green > blue) ? green[7:0] : blue[7:0];						
+assign min = (red < green)? ((red<blue) ? red[7:0] : blue[7:0]) : (green < blue) ? green [7:0] : blue[7:0];
+assign saturation = (value - min)* 255 / value;
+assign hue = (red == green && red == blue) ? 0 :((value != red)? (value != green) ? (((240*((value - min))+ (60* (red - green)))/(value-min))>>1):
+                ((120*(value-min)+60*(blue - red))/(value - min)>>1): 
+                (blue < green) ? ((60*(green - blue)/(value - min))>>1): (((360*(value-min) +(60*(green - blue)))/(value - min))>>1));
+
+///Detect Ping Pong balls
+reg prev_detect_high, prev_high;
+wire red_ball_detect, green_ball_detect, orange_ball_detect, grey_ball_detect, blue_ball_detect;
+assign red_ball_detect = (((hue >= 0 && hue <= 15)||(hue >= 165 && hue <= 180)) && value > 120 && saturation > 110);
+//assign green_ball_detect = (hue >= 45 && hue <= 80 && value > 100 && saturation > 110);
+assign orange_ball_detect = (hue >= 15 && hue <= 32 && value > 120 && saturation > 110);
+assign blue_ball_detect = (hue >= 50 && hue <= 150 && value > 140 && saturation > 90);		
+//assign grey_ball_detect = (value < 80 && saturation < 70);			 
+initial begin
+	prev_detect_high <= 0;
+	prev_high <= 0;
+end
+
+always@(negedge clk) begin
+	prev_high <= prev_detect_high;
+	prev_detect_high <= ((green_ball_detect || orange_ball_detect || blue_ball_detect || red_ball_detect || grey_ball_detect));
+end
+
 // Highlight detected areas
-wire [23:0] red_high;
+wire [23:0] color_high;
 assign grey = green[7:1] + red[7:2] + blue[7:2]; //Grey = green/2 + red/4 + blue/4
-assign red_high  =  red_detect ? {8'hff, 8'h0, 8'h0} : {grey, grey, grey};
+//assign red_high  =  red_detect ? {8'hff, 8'h0, 8'h0} : {grey, grey, grey};
+assign color_high  =  (red_ball_detect && prev_detect_high && prev_high) ? {8'hff,8'h10,8'h0} 
+	: ((green_ball_detect && prev_detect_high && prev_high)? {8'h04,8'hbd,8'h42} 
+	: ((orange_ball_detect && prev_detect_high && prev_high)? {8'hea,8'h9e,8'h1b} 
+	: ((blue_ball_detect && prev_detect_high && prev_high) ? {8'h0,8'h0,8'hff}
+	: ((grey_ball_detect && prev_detect_high && prev_high) ? {8'h0,8'h0,8'h0}
+	: {grey,grey,grey})) )) ;
 
 // Show bounding box
 wire [23:0] new_image;
 wire bb_active;
 assign bb_active = (x == left) | (x == right) | (y == top) | (y == bottom);
-assign new_image = bb_active ? bb_col : red_high;
+assign new_image = bb_active ? bb_col : color_high;
 
 // Switch output pixels depending on mode switch
 // Don't modify the start-of-packet word - it's a packet discriptor
@@ -121,7 +159,7 @@ end
 //Find first and last red pixels
 reg [10:0] x_min, y_min, x_max, y_max;
 always@(posedge clk) begin
-	if (red_detect & in_valid) begin	//Update bounds when the pixel is red
+	if (((green_ball_detect || orange_ball_detect || blue_ball_detect || red_ball_detect || grey_ball_detect) && prev_detect_high && prev_high) & in_valid) begin	//Update bounds when the pixel is red
 		if (x < x_min) x_min <= x;
 		if (x > x_max) x_max <= x;
 		if (y < y_min) y_min <= y;
@@ -141,28 +179,28 @@ reg [10:0] left, right, top, bottom;
 reg [7:0] frame_count;
 always@(posedge clk) begin
 	if (eop & in_valid & packet_video) begin  //Ignore non-video packets
-		
+
 		//Latch edges for display overlay on next frame
 		left <= x_min;
 		right <= x_max;
 		top <= y_min;
 		bottom <= y_max;
-		
-		
+
+
 		//Start message writer FSM once every MSG_INTERVAL frames, if there is room in the FIFO
 		frame_count <= frame_count - 1;
-		
+
 		if (frame_count == 0 && msg_buf_size < MESSAGE_BUF_MAX - 3) begin
 			msg_state <= 2'b01;
 			frame_count <= MSG_INTERVAL-1;
 		end
 	end
-	
+
 	//Cycle through message writer states once started
 	if (msg_state != 2'b00) msg_state <= msg_state + 2'b01;
 
 end
-	
+
 //Generate output messages for CPU
 reg [31:0] msg_buf_in; 
 wire [31:0] msg_buf_out;
@@ -285,20 +323,20 @@ begin
 	   s_readdata <= {32'b0};
 		read_d <= 1'b0;
 	end
-	
+
 	else if (s_chipselect & s_read) begin
 		if   (s_address == `REG_STATUS) s_readdata <= {16'b0,msg_buf_size,reg_status};
 		if   (s_address == `READ_MSG) s_readdata <= {msg_buf_out};
 		if   (s_address == `READ_ID) s_readdata <= 32'h1234EEE2;
 		if   (s_address == `REG_BBCOL) s_readdata <= {8'h0, bb_col};
 	end
-	
+
 	read_d <= s_read;
 end
 
 //Fetch next word from message buffer after read from READ_MSG
 assign msg_buf_rd = s_chipselect & s_read & ~read_d & ~msg_buf_empty & (s_address == `READ_MSG);
-						
+
 
 
 endmodule
